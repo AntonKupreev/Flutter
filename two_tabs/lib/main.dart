32117c0ae28eccdcbf83a46fcaa8367fc9157376
Anton <madrid.87@mail.ru>
2022-10-05 21:34:22 +0300

    2 tast

--- two_tabs/lib/main.dart
+++ two_tabs/lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -27,58 +26,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> nav = ['Список 111', 'Список 2'];
-  final List fakeData = List.generate(100, (index) => index.toString());
-
-  Map<String, dynamic> data = {
-    'Мои фото': [
-      'https://picsum.photos/1200/501',
-      'https://picsum.photos/1200/502',
-      'https://picsum.photos/1200/503',
-      'https://picsum.photos/1200/504',
-      'https://picsum.photos/1200/505',
-      'https://picsum.photos/1200/506',
-      'https://picsum.photos/1200/507',
-      'https://picsum.photos/1200/508',
-      'https://picsum.photos/1200/509',
-      'https://picsum.photos/1200/510',
-    ],
-    'Галерея': [
-      'https://picsum.photos/1200/511',
-      'https://picsum.photos/1200/512',
-      'https://picsum.photos/1200/513',
-      'https://picsum.photos/1200/514',
-      'https://picsum.photos/1200/515',
-      'https://picsum.photos/1200/516',
-      'https://picsum.photos/1200/517',
-      'https://picsum.photos/1200/518',
-      'https://picsum.photos/1200/519',
-      'https://picsum.photos/1200/520',
-    ]
-  };
-  List names = [];
+  var listData = List.generate(50, (index) => index);
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: data.keys.length,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Homework example'),
-          bottom: TabBar(
-              tabs: (data.keys.map((String k) => Tab(text: k)).toList())),
-        ),
-        body: TabBarView(
-          children: data.keys.map((name) {
-            return ListView(
-              key: PageStorageKey(name),
-              children: <Widget>[
-                ...data[name].map((e) {
-                  return Image.network(e);
-                }).toList()
-              ],
-            );
-          }).toList(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'List view divider',
+            ),
+          ],
         ),
       ),
     );

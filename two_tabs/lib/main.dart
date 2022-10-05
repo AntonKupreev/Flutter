8ead7daa9f60a073ef0b700f8312b9368c387d53
Anton <madrid.87@mail.ru>
2022-10-05 21:00:25 +0300

    new project

--- two_tabs/lib/main.dart
+++ two_tabs/lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> nav = ['Список 1', 'Список 2'];
+  final List<String> nav = ['Список 111', 'Список 2'];
   final List fakeData = List.generate(100, (index) => index.toString());
 
   Map<String, dynamic> data = {
